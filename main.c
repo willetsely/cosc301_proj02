95bc94c6c095c8dc3ce7ee1d6d829fbe2e4415a2
Joel Sommers <jsommers@acm.org>
2013-09-24 17:17:32 -0400

    Initial commit of proj02 template

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <poll.h>
+#include <signal.h>
+
+
+int main(int argc, char **argv) {
+
+    printf("main.c, so lonely.  no code here yet :-(\n");
+
+    return 0;
+}
+
