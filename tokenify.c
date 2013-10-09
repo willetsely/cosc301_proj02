188c2a1324ce71b0a24e549f949d3aa655658334
Markos <Markos@macbookpro-6eab.colgate.edu>
2013-10-09 13:25:57 -0400

    updated tokenify.c

--- tokenify.c
+++ tokenify.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
+#include "tokenify.h"
 
 char *tokenify(const char *str, int size, int switch_value)
 {
@@ -13,37 +13,21 @@ char *tokenify(const char *str, int size, int switch_value)
 	    const char *sep = " \n\t"; //switch_value == 1, tokenify breaks up commands by spaces
 	    break;
     }
-    char *result = 
+    char *result = (char *)malloc(sizeof(char *) * size); 
     char *s = strdup(str);
     char *word, *temp;
+    int i = 0;
     for (word = strtok_r(s, sep, &temp); word != NULL; word = strtok_r(NULL, sep, &temp))
     {
-	if (word == '#')
-	    return;
-	int integer = 1;
-	int i = 0;
-	int number;
-	int number_check = 0;
-	while (integer)
+	if (word[0] == '#')
 	{
-	    if ( i == 0 && word[i] == '-')
-		i++;
-	    if (isdigit(word[i]) == 0)
-	        integer = 0;
-	    else
-	    {
-	        i++;
-		if (word[i] = '\0')
-		{
-		    number = atoi(word);
-		    number_check = 1;
-		    integer = 0;
-		}
-	    }
+	    result[i] = NULL;
+	    return result;
 	}
-	if (number_check == 1) 
-	    sorted_insert(number, *head);
+	result[i] = word;
+	i++;
     }
+    result[i] = NULL;
     free(s);
-    return;
+    return result;
 }
