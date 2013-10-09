579d76523ae842520e349fe6eb52d8422057dc7b
druckcity <drucker.daniel@hotmail.com>
2013-10-09 12:29:38 -0400

    message

---
+++ parallel.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <ctype.h>
+#include "parallel.h"
+
+int parallel(char *cmd)
+{
+
+
+}
