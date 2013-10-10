92026de80f06ad121eb5d24c5ce638dfe1069d82
willetsely <willestely@gmail.com>
2013-10-09 22:06:12 -0400

    Still need to work out how to read EOF

--- main.c
+++ main.c
@@ -20,29 +20,39 @@ int main(int argc, char **argv)
 {
 	const char * prompt = ">>>";
 	int moreinput = 1;
-	int mode = 0;	//mode = 0 for sequential, 1 for parallel
-	int modechange = 0; //modechange tracks if a change in mode needs to be made at the end of the command line
+	int mode = 0;	//mode = 0 for sequential, 1 for parallel, 3 to exit
+
 	while(moreinput)
     	{
 		printf("%s",prompt);
 		//fflush
-		char input [1024];
-		fgets(input, 1024, stdin);
-		int comcount = 1; 	//start at 1 because there is one more command than there are semicolons
-		int i = 0;
-		for(;input[i] != "\0"; i++)
-		{
-			if(input[i] == ";")
-				comcount++;
+		char input [1024];	
+		fgets(input, 1024, stdin); //get input
 
+		int comcount = 1; //start at 1 because there is one more command than there are semicolons
+		int i = 0;
+		for(;input[i] != '\0'; i++)  //this loop counts the number of commands
+		{
+			if(input[i] == ';')
+				comcount++;
 		}
-		char * argc[comcount] = tokenify(input, comcount, 0) //the 0 indicates prompt, ie split on semicolons
-		for(size of argc)
-			if(mode == 0)
-				//sequential execution function
-			if(mode == 1)
-				//parallel execution function
-			//both executions should return an int for modechange
+
+		char * argc[comcount] = tokenify(input, 0)//the 0 indicates prompt, ie split on semicolons
+
+		if(mode == 0) //start sequential execution
+		{
+			int j = 0;
+			for(;j < comcount; j++)
+			{
+		        mode = sequential(*argc[j], mode) //mode is set equal because sequential returns what mode should be for the next command line
+			}
+		}
+
+		else if(mode == 1) //start parallel execution
+			//parallel execution function
+
+        if(mode == 3) //if an exit command has been issued, return
+            return 0;
 	}
 
     return 0;
