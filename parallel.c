a6103defcff03f5418855a528826a5328c295027
druckcity <drucker.daniel@hotmail.com>
2013-10-09 22:41:51 -0400

    Updated parallel.c and sequential.c

--- parallel.c
+++ parallel.c
@@ -5,9 +5,28 @@
 #include <strings.h>
 #include <ctype.h>
 #include "parallel.h"
+#include "tokenify.h"
+#include "mode.h"
 
 int parallel(char *cmd)
 {
+    char *cmd[] = tokenify(cmd, 1);
 
+    int childrv;
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+     /***  Run Child Process  ***/
+        if (execv(cmd[0], cmd) < 0) {
+            fprintf(stderr, "execv failed: %s\n", strerror(errno));
+        }
+        printf("\n");
+    } 
+    else if (pid > 0)
+    {
+     /***  Wait for Child Process to finish then run Parent ***/
+        waitpid(pid, &childrv, 0);
+        printf("~~~~~~~~~~~Child Process Finished!~~~~~~~~~~~~\n");
+    }
 
 }
--- sequential.c
+++ sequential.c
@@ -5,9 +5,37 @@
 #include <strings.h>
 #include <ctype.h>
 #include "sequential.h"
+#include "tokenify.h"
+#include "mode.h"
 
-int sequential(char *cmd)
+int sequential(char *cmd, int mode)
 {
+    char *cmd[] = tokenify(cmd, 1);
+    if (cmd[0] == "mode" && mode != 3 && cmd[2] == NULL)
+    {
+        mode = mode(cmd[1], mode);
+    }
+    if (cmd[0] == "exit" && cmd[2] == NULL)
+    {
+        mode = 3;
+    }
 
+    int childrv;
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+     /***  Run Child Process  ***/
+        if (execv(cmd[0], cmd) < 0) {
+            fprintf(stderr, "execv failed: %s\n", strerror(errno));
+        }
+        printf("\n");
+    } 
+    else if (pid > 0)
+    {
+     /***  Wait for Child Process to finish then run Parent ***/
+        waitpid(pid, &childrv, 0);
+        printf("~~~~~~~~~~~Child Process Finished!~~~~~~~~~~~~\n");
+    }
 
 }
+
