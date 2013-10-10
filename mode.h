baf94344b3d866083b799d8daf0111dc31cc94db
willetsely <willestely@gmail.com>
2013-10-10 14:29:51 -0400

    Removed function files. All funtions now in the functions.c file

--- mode.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MODE_H__
-#define __MODE_H__
-
-
-int mode_funct(const char *command, int mode_type)
-
-#endif // __MODE_H__
--- tokenify.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TOKENIFY_H__
-#define __TOKENIFY_H__
-
-
-char *tokenify(const char *str, int switch_value)
-
-#endif // __TOKENIFY_H__
